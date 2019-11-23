ee524ee0175ffb1b2b5d83b7b25193f0246b67a5
iamgkstack <gauravkumar5295@gmail.com>
2019-11-23 18:54:50 +0530

    minor changes

--- tests/app/dummy.test.js
+++ tests/app/dummy.test.js
@@ -3,7 +3,7 @@ const expect = require('expect');
 const ressultTrue = require('./../../app/dummy');
 
 describe('resultTrue', () => {
-    it('should return true', () => {
+    it('should return true if result exist', () => {
         expect(ressultTrue()).toBe(true);
     })
 })
